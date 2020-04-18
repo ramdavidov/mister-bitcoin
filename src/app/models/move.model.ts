9df5914ee5455b86def4d0aa42b1366d6e1df98b
Rami Davidov <rami.davidov@gmail.com>
2020-04-18 15:38:42 +0300

    Auto-generated commit

--- src/app/models/move.model.ts
+++
@@ -1,9 +0,0 @@
-export class Move {
-
-    constructor(public toId: string, public to: string = '', public at?: number, public amount: number = 0) {
-    }
-
-    setAt?() {
-        this.at = Date.now()
-    }
-}
